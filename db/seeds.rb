6197650ef95f5c25bace40662964cfdbbde35abb
JTSwisher <jefftswisher@gmail.com>
2020-03-02 11:55:54 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create(title: "Test Title", description: "Test description for test post.")
